b1192c17a3b03888c1125684b5053ed7f04eb0cf
liangzw7 <1901543053@qq.com>
2020-10-10 18:51:53 +0800

    webpack-dev-server配置

--- src/index.js
+++ src/index.js
@@ -6,14 +6,21 @@ import '@/styles/css/reset.css';
 import indexStyle from '@/styles/css/index.css';
 
 import indexLessStyle from '@/styles/less/index.less';
-
 console.log(indexLessStyle);
+console.log('module.hot', module.hot);
+let num = 1;
 window.addEventListener('DOMContentLoaded', function() {
-	// var img = document.createElement('img');
+	var h = document.createElement('h6');
 	const img = new Image();
 	img.src = Logo;
 	img.classList.add(indexLessStyle.pic);
+	h.innerText = num;
+	h.onclick = function() {
+		h.innerText = ++num;
+	};
 	console.log(img);
+	console.log(h);
 	// 挂在dom
 	document.body.appendChild(img);
+	document.body.appendChild(h);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,39 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].[chunkhash:6].js'
+		filename: '[name].[hash:6].js'
+	},
+	//开启devServe
+	devServer: {
+		// open: true, //自动打开浏览器页面
+		// openPage: 'smarttitle',
+		overlay: true,
+		compress: true, //启用gzip压缩
+		host: '0.0.0.0', //指定host，默认localhost。如果希望外部服务器可访问，可指定为：host: '0.0.0.0'
+		port: 8989, // 端口
+		hot: true, //启用 webpack 的模块热替换特性
+		// hotOnly: true, // 可选，即使代码没生效，也不刷新浏览器
+		// https: true, //启用https服务
+		// index: 'index.html',
+		// inline: false, //默认为true，启用内联模式，false对应iframe模式
+
+		// contentBase: path.join(__dirname, 'public') // 告诉服务器从哪里提供内容。只有在你想要提供静态文件时才需要。
+		// 在所有响应中添加首部内容
+		// headers: {
+		// 	'X-Custom-Foo': 'bar'
+		// }
+		//如果你有单独的后端开发服务器 API，并且希望在同域名下发送 API 请求 ，那么代理某些 URL 会很有用。
+		proxy: {
+			'/api': {
+				target: 'http://localhost: 3000',
+				pathRewrite: { '^/api': '' },
+				secure: false //默认情况下，不接受运行在 HTTPS 上，且使用了无效证书的后端服务器。如果你想要接受，修改配置如下
+			}
+		},
+		publicPath: '/cdn/', //此路径下的打包文件可在浏览器中访问。
+		// stats: 'errors-only',
+		useLocalIp: true, //使用本地ip启动项目
+		historyApiFallback: true //当使用 HTML5 History API 时，任意的 404 响应都可能需要被替代为 index.html
 	},
 	module: {
 		rules: [
@@ -19,7 +51,7 @@ module.exports = {
 						loader: 'url-loader',
 						options: {
 							// 当图片大于801k时，交给file-loader处理，否则url-loader会把图片src转成base64编码
-							limit: 801920,
+							limit: 920,
 							// 打包后的文件名
 							name: '[name].[hash:8].[ext]',
 							// 打包路径
@@ -35,7 +67,7 @@ module.exports = {
 					{
 						loader: 'css-loader',
 						options: {
-							modules: true
+							modules: false
 						}
 					},
 					'postcss-loader'
